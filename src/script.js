2992264d8672025c2e319ade7b865ad5ed90b46f
Yahor Neverouski <hardstyle6537@gmail.com>
2020-04-22 20:55:24 +0300

    feat: add generation words

--- src/script.js
+++ src/script.js
@@ -1,8 +1,39 @@
-const getWords = async (page, group) => {
-  const url = `https://afternoon-falls-25894.herokuapp.com/words?page=${page}&group=${group}`;
-  const res = await fetch(url);
-  const json = await res.json();
-  console.log(JSON.stringify(json, null, 1));
-};
+class Words {
+  constructor() {
+    this.name = 'words';
+    this.currentGroup = '0';
+  }
 
-getWords(1, 1)
+  createDOM() {
+    this.startButton = document.querySelector('.start-btn');
+    this.blockWords = document.querySelector('.block-words');
+  }
+
+  addEventListener() {
+    this.startButton.addEventListener('click', () => this.getWords());
+  }
+
+  async getWords() {
+    const page = Math.floor(Math.random() * (29 + 1));
+    const url = `https://afternoon-falls-25894.herokuapp.com/words?page=${page}&group=${this.currentGroup}`;
+    const res = await fetch(url);
+    const json = await res.json();
+    this.createWords(json);
+  }
+
+  async createWords(json) {
+    for (let i = 0; i < this.blockWords.children.length; i += 1) {
+      const url = `https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20200422T174219Z.a6338781f9423192.28f2896df71fd9376630fd91e64a03518d511f99&text= ${json[i].word} &lang=en-ru`;
+      const res = await fetch(url);
+      const data = await res.json();
+      this.blockWords.children[i].querySelector('.word').textContent = json[i].word;
+      this.blockWords.children[i].querySelector('.transcription').textContent = json[i].transcription;
+      this.blockWords.children[i].querySelector('.translate').textContent = data.text;
+    }
+  }
+}
+
+
+const words = new Words();
+words.createDOM();
+words.addEventListener();
