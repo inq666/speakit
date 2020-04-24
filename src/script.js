5a75e2e08c77a570cbf6ad6372f1e2fef2e46869
Yahor Neverouski <hardstyle6537@gmail.com>
2020-04-25 00:41:02 +0300

    feat: add statistics && results

--- src/script.js
+++ src/script.js
@@ -3,28 +3,137 @@ class Words {
     this.audio = new Audio();
     this.audio.autoplay = true;
     this.gameMode = false;
-    this.currentGroup = '0';
+    this.currentGroup = '00';
     window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
     this.recognition = new window.SpeechRecognition();
+    this.statisticsInfo = JSON.parse(localStorage.getItem('statistics')) || [];
   }
 
   createDOM() {
     this.wordImage = document.querySelector('.word-image');
     this.startButton = document.querySelector('.start-btn');
+    this.resultButton = document.querySelector('.result-btn');
+    this.newGameButton = document.querySelector('.results-new-game');
+    this.backButton = document.querySelector('.stat-back');
+    this.newGameButton = document.querySelector('.new-game-btn');
     this.blockWords = document.querySelector('.block-words');
     this.stringWord = document.querySelector('.word-string');
     this.speakButton = document.querySelector('.speak-btn');
     this.menu = document.querySelector('.menu');
+    this.results = document.querySelector('.results');
+    this.guessedContainer = document.querySelector('.guessed-container');
+    this.unsolvedContainer = document.querySelector('.unsolved-container');
+    this.countGuess = document.querySelector('.count-guessed');
+    this.countUnsolved = document.querySelector('.count-unsolved');
+    this.returnButton = document.querySelector('.results-return');
+    this.statistics = document.querySelector('.statistics');
+    this.statisticsButton = document.querySelector('.statistics-btn');
+    this.itemStatistics = document.querySelector('.item-statistics');
   }
 
   addEventListener() {
+    this.newGameButton.addEventListener('click', () => this.newGame());
+    this.backButton.addEventListener('click', () => this.backStatistics());
+    this.statisticsButton.addEventListener('click', () => this.openStatistics());
+    this.newGameButton.addEventListener('click', () => this.newGame());
     this.menu.addEventListener('click', (event) => this.selectcCmplexity(event));
     this.speakButton.addEventListener('click', () => this.startGame());
+    this.returnButton.addEventListener('click', () => this.returnGame());
     this.startButton.addEventListener('click', () => this.getWords());
-    this.blockWords.addEventListener('click', (event) => this.audioWord(event));
+    this.resultButton.addEventListener('click', () => this.openResult());
+    window.addEventListener('click', (event) => this.audioWord(event));
+    window.addEventListener('unload', () => {
+      localStorage.setItem('statistics', JSON.stringify(this.statisticsInfo));
+    });
     this.recognition.addEventListener('result', (event) => this.recording(event));
-    this.recognition.addEventListener('end', this.recognition.start);
+    this.recognition.addEventListener('end', () => {
+      if (this.gameMode) {
+        this.recognition.start();
+      }
+    });
   }
+
+  backStatistics() {
+    this.statistics.style.display = 'none';
+    if (!this.statisticsInfo.length) return;
+    document.querySelector('.statistics-modal-window').innerHTML = '';
+  }
+
+  openStatistics() {
+    this.statistics.style.display = 'block';
+    for (let i = 0; i < this.statisticsInfo.length; i += 1) {
+      if (i === 8) return;
+      if (this.statisticsInfo.length) {
+        document.querySelector('.statistics-text').style.display = 'none';
+      }
+      const newItem = this.itemStatistics.cloneNode(true);
+      newItem.querySelector('.date').textContent = (this.statisticsInfo[i].date).slice(0, 25);
+      newItem.querySelector('.stat-guess').textContent = `Guessed: ${this.statisticsInfo[i].guessed}`;
+      newItem.querySelector('.stat-unsolved').textContent = `Unsolved ${this.statisticsInfo[i].unsolved}`;
+      document.querySelector('.statistics-modal-window').append(newItem);
+    }
+    this.itemStatistics.remove();
+  }
+
+  newGame() {
+    const activeWord = document.querySelector('.item-word-active');
+    if (activeWord !== null) {
+      activeWord.classList.remove('item-word-active');
+    }
+    this.gameMode = false;
+    this.returnGame(true);
+    this.getWords();
+    this.resetStat();
+  }
+
+  returnGame(mode) {
+    this.results.style.display = 'none';
+    this.results.querySelectorAll('.item-word').forEach((item) => {
+      item.classList.remove('words-results');
+      this.blockWords.append(item);
+      if (!mode) {
+        if (item.dataset.guess === 'true') {
+          item.classList.add('guessed-word');
+        }
+      }
+    });
+  }
+
+  resetStat() {
+    document.querySelector('.stars').style.opacity = '0';
+    document.querySelectorAll('.star-win').forEach((item) => {
+      item.classList.remove('star-win');
+      item.classList.add('star-lose');
+    });
+    this.gameMode = false;
+    this.stringWord.textContent = '';
+    this.stringWord.classList.remove('play-game');
+    this.recognition.stop();
+    const obj = {
+      guessed: this.correctly,
+      unsolved: this.unsolved,
+      date: `${new Date()}`,
+    };
+    this.statisticsInfo.unshift(obj);
+  }
+
+  openResult() {
+    if (this.correctly === 10) {
+      this.resetStat();
+    }
+    this.results.style.display = 'block';
+    this.blockWords.querySelectorAll('.item-word').forEach((item) => {
+      item.classList.add('words-results');
+      if (item.classList.contains('guessed-word')) {
+        this.guessedContainer.append(item);
+        item.classList.remove('guessed-word');
+        item.dataset.guess = true;
+      } else {
+        this.unsolvedContainer.append(item);
+      }
+    });
+  }
+
 
   selectcCmplexity(event) {
     const targetChain = event.target;
@@ -37,12 +146,20 @@ class Words {
       this.blockWords.querySelectorAll('.item-word').forEach((item) => {
         item.classList.remove('item-word-active');
       });
-      this.currentGroup = targetChain.dataset.level - 1;
+      this.currentGroup = targetChain.dataset.level;
       this.getWords();
+      this.resetStat();
     }
   }
 
   startGame() {
+    if (this.gameMode) return;
+    const activeWord = document.querySelector('.item-word-active');
+    if (activeWord !== null) {
+      activeWord.classList.remove('item-word-active');
+    }
+    this.correctly = 0;
+    this.unsolved = 10;
     this.recognition.start();
     this.gameMode = true;
     this.stringWord.textContent = '';
@@ -57,11 +174,25 @@ class Words {
       this.stringWord.textContent = transcript;
       Array.from(this.blockWords.children).forEach((item) => {
         if (item.querySelector('.word').textContent === transcript) {
-          item.classList.add('guessed-word');
+          const currentNumber = item.dataset.number;
+          let currentPage = this.page + 1;
+          if (currentPage < 10) {
+            currentPage = `0${currentPage}`;
+          }
+          this.correctly += 1;
+          this.unsolved -= 1;
           this.audio.src = 'audio/correct.mp3';
+          this.wordImage.src = `https://raw.githubusercontent.com/inq666/rslang-data/master/data/${currentPage}_${currentNumber}.jpg`;
+          this.countGuess.textContent = `${this.correctly}`;
+          this.countUnsolved.textContent = `${this.unsolved}`;
+          item.classList.add('guessed-word');
           const newStar = document.querySelector('.star-lose');
           newStar.classList.add('star-win');
           newStar.classList.remove('star-lose');
+          if (this.correctly === 10) {
+            this.audio.src = 'audio/success.mp3';
+            setTimeout(() => this.openResult(), 500);
+          }
         }
       });
     }
@@ -102,8 +233,12 @@ class Words {
     const targetWord = event.target.closest('.item-word');
     if (targetWord !== null) {
       const currentNumber = targetWord.dataset.number;
-      this.wordImage.src = `https://raw.githubusercontent.com/inq666/rslang-data/master/data/${this.page + 1}_${currentNumber}.jpg`;
-      this.audio.src = `https://raw.githubusercontent.com/inq666/rslang-data/master/data/${this.page + 1}_${currentNumber}.mp3`;
+      let currentPage = this.page + 1;
+      if (currentPage < 10) {
+        currentPage = `0${currentPage}`;
+      }
+      this.wordImage.src = `https://raw.githubusercontent.com/inq666/rslang-data/master/data/${currentPage}_${currentNumber}.jpg`;
+      this.audio.src = `https://raw.githubusercontent.com/inq666/rslang-data/master/data/${currentPage}_${currentNumber}.mp3`;
       this.stringWord.textContent = targetWord.querySelector('.translate').textContent.toUpperCase();
       this.blockWords.querySelectorAll('.item-word').forEach((item) => {
         item.classList.remove('item-word-active');
